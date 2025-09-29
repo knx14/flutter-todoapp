3619f59b89a67d7752e4543d122748a959a2a412
Kanato Maenishi <kanato.maenishi@gmail.com>
2025-09-29 14:07:21 +0900

    Initial commit: Flutter ToDo app with shared_preferences and uuid packages

---
+++ lib/models/todo_item.dart
@@ -0,0 +1,30 @@
+import 'package:uuid/uuid.dart';
+
+class TodoItem {
+    //プロパティの定義
+    final String id;//finalは変更不可の変数
+    String title;
+    bool isDone;//boolは真偽値
+
+    //コンストラクタの定義
+    TodoItem({String? id, required this.title, this.isDone = false}) 
+        : id = id ?? const Uuid().v4();
+
+//データの永続化のための変換メソッド
+//TodoItemをMap<String, dynamic>に変換するメソッド
+Map<String, dynamic> toMap() {
+    return {
+        'id': id,
+        'title': title,
+        'isDone': isDone,
+    };
+}
+//MapからTodoItemオブジェクトを生成する
+factory TodoItem.fromJson(Map<String, dynamic> json) {
+    return TodoItem(
+        id: json['id'],
+        title: json['title'],
+        isDone: json['isDone'],
+    );
+}
+}
